b026834885cf62108ebafcee41ac0b31468ba33f
Silvester Sabathiel <ssabathiel@gmail.com>
2021-04-06 15:22:22 +0200

    states, actions, interaction for SingleAgent

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,9 @@ This file contains some utility functions used in different classes.
 
 import torch
 from torch import nn
+from PIL import Image, ImageDraw
+#from fonts.ttf import AmaticSC
+
 
 def initialize_weights(layer):
     """Initialize a layer's weights and biases.
@@ -23,26 +26,63 @@ def initialize_weights(layer):
             pass 
 
 
-def concat_imgs_h(img_list):
+def concat_imgs_h(img_list, dist=0):
     total_img = img_list[0]
-    for img_i in img_list:
-        total_img = concat_2_imgs_h(total_img, img_i)
+    for i in range(1, len(img_list)):
+        total_img = concat_2_imgs_h(total_img, img_list[i], dist=dist)
     return total_img
 
-def concat_imgs_v(img_list):
+def concat_imgs_v(img_list, dist=0):
     total_img = img_list[0]
-    for img_i in img_list:
-        total_img = concat_2_imgs_v(total_img, img_i)
+    for i in range(1, len(img_list)):
+        total_img = concat_2_imgs_v(total_img, img_list[i], dist=dist)
     return total_img
 
-def concat_2_imgs_h(im1, im2):
-    dst = Image.new('RGB', (im1.width + im2.width, im1.height), color='white')
+def concat_2_imgs_h(im1, im2, dist=0):
+    dst = Image.new('RGB', (im1.width + dist + im2.width, im1.height), color='white')
     dst.paste(im1, (0, 0))
-    dst.paste(im2, (im1.width, 0))
+    dst.paste(im2, (im1.width + dist, 0))
     return dst
 
-def concat_2_imgs_v(im1, im2):
-    dst = Image.new('RGB', (im2.width, im1.height + im2.height), color='white')
+def concat_2_imgs_v(im1, im2, dist=0):
+    dst = Image.new('RGB', (im2.width, im1.height + dist + im2.height), color='white')
     dst.paste(im1, (0, 0))
-    dst.paste(im2, (0, im1.height))
+    dst.paste(im2, (0, im1.height + dist))
     return dst
+
+
+def annotate_below(imgy, text):
+    text_img = Image.new("RGBA", (int(imgy.width), int(imgy.height/4)), (255, 255, 255, 0))
+    text_draw = ImageDraw.Draw(text_img)
+    # font = ImageFont.load("arial.ttf")
+    text_draw.text((0, 0), text, fill=0)
+    imgy = concat_imgs_v([imgy, text_img])
+    return imgy
+
+def annotate_nodes(imgy, text_list):
+    n_nodes = len(text_list)
+    text_img = Image.new("RGBA", (int(2*imgy.width), int(imgy.height)), (255, 255, 255, 0))
+    text_draw = ImageDraw.Draw(text_img)
+    for n in range(n_nodes):
+        # font = ImageFont.load("arial.ttf")
+        text_draw.text((0, int((n+0.5)*imgy.height/len(text_list))), text_list[n], fill=0)
+    imgy = concat_imgs_h([imgy, text_img])
+    return imgy
+
+def add_grid_lines(_img, _array):
+    img_width = _img.width
+    img_height = _img.height
+    array_width = _array[0, :].size
+    array_height = _array[:, 0].size
+    draw = ImageDraw.Draw(_img)
+    linewidth = 2
+    for x in range(array_width + 1):
+        line = ((x * img_width / array_width - int(linewidth / 2), 0),
+                (x * img_width / array_width - int(linewidth / 2), img_height))
+        draw.line(line, fill=0, width=2)
+    for y in range(array_height + 1):
+        line = ((0, y * img_height / array_height - int(linewidth / 2)),
+                (img_width, y * img_height / array_height - int(linewidth / 2)))
+        draw.line(line, fill=0, width=2)
+    return _img
+
